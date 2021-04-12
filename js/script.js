9af2f35291e8d56d69971cfbc19903ad81b70235
cadu <cadu200761@gmail.com>
2021-04-12 17:22:12 -0300

    d20

--- js/script.js
+++ js/script.js
@@ -9,3 +9,20 @@ function inutil(x) {
 function alerta() {
     alert(inutil(x))
 }
+
+function dado20(){
+    let dado = Math.floor(Math.random() * 20) + 1 
+    console.log(dado)
+    if (dado === 1){
+        $('#d1').modal('show')
+       } else if(dado >= 2 && dado <= 5){
+        $('#d6').modal('show')
+       } else if(dado >= 6 && dado <= 12){
+        $('#d12').modal('show')
+      } else if(dado >= 13 && dado <= 19){
+        $('#d19').modal('show')
+      } else if(dado = 20){
+        $('#d20').modal('show')
+      }
+}
+
